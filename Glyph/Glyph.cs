cd3dc1ee82520c4f36b30194e70f0adf533d0376
dedouwe26 <63008025+dedouwe26@users.noreply.github.com>
2024-02-14 10:11:02 +0100

    begin

---
+++ Glyph/Glyph.cs
@@ -0,0 +1,14 @@
+namespace Glyph
+{
+    public class Glyph
+    {
+        public bool exists = false;
+        public string toText() {
+            
+        }
+    }
+
+    public class Selection {
+        public bool selected = false;
+    }
+}
---
+++ Glyph/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Glyph {
+    public static class Program
+    {
+        public static Glyph currentGlyph = new();
+        public static void Main(string[] args) {
+            Console.WriteLine("Press ESC to stop");
+        }
+
+        public static void Update() {
+
+        }
+        public static void OnCommand() {
+
+        }
+    }
+}
