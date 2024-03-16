7b770db9c0e441d24725bec1a21db6ff894826a0
dedouwe26 <lucdouwe@gmail.com>
2024-03-16 10:24:17 +0100

    Wrong version!

--- Glyph/AssemblyInfo.cs
+++ Glyph/AssemblyInfo.cs
@@ -1,8 +1,8 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("1.1.0.0")]  
-[assembly: AssemblyFileVersion("1.1.0.0")]
-[assembly: AssemblyInformationalVersion("1.1.0.0")]
+[assembly: AssemblyVersion("1.2.0.0")]  
+[assembly: AssemblyFileVersion("1.2.0.0")]
+[assembly: AssemblyInformationalVersion("1.2.0.0")]
 [assembly: AssemblyDescription("Glyph is a console text editor with text decorations.")]
 [assembly: AssemblyTitle("Glyph")]
 [assembly: AssemblyProduct("Glyph")]
